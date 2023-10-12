b083354ab45ecac6ec6d44c14d6d91ea0294a02b
zikglobal <125207118+zikglobal@users.noreply.github.com>
2023-10-12 10:50:11 +0100

    Add project files.

---
+++ CompanyEmployees/Extentions/ServiceExtentions.cs
@@ -0,0 +1,18 @@
+﻿namespace CompanyEmployees.Extentions
+{
+    public static class ServiceExtentions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+        public static void ConfigureIISIntegration(this IServiceCollection services) => services.Configure<IISOptions>(options =>
+        {
+
+        });
+    }
+}
---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,32 @@
+using CompanyEmployees.Extentions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+if(app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
